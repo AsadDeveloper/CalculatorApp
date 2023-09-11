aec1f3f1f14d4bf4fa00c982ee96b744176f45ef
AsadDeveloper <asad41762@gmail.com>
2023-09-11 22:19:38 +0500

    commit

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color whiteColor = Colors.white;
+const greenColor = Colors.blue;
+const Color greyColor = Colors.blueGrey;
+
+const headingTextStyle = TextStyle(color: Colors.white);
